a3ea467dfbe8d585936ae94736577871b3697c70
GajendraKaushik <119392005+GajendraKaushik@users.noreply.github.com>
2025-06-25 16:55:43 +0000

    Update: Updated the navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ function App() {
 
   return (
     <>
-     <div>
-     <Navbar/>
-     <Profile/>
+      <div className='min-h-screen bg-gradient-to-b from gray-900 to-gray-950 text-white'>
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900/20 via-transparent to-transparent">
+          <Navbar />
+          <main className='pt-20'>
+            {/* <section>
+            <Profile/>
+          </section> */}
 
-     </div>
+          </main>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,18 +1,28 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-const Navbar = ()=> {
-    return (
-    <div className='w-full bg-black py-7 flex justify-between'>
-      <div className='text-white text-2xl ml-2'>
-        GAJENDRA
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full bg-black/50 backdrop-blur-lg z-50">
+      <div className="mx-2 px-2 sm:px-16 py-4">
+        <div className="flex justify-between items-center">
+          <a href='/'>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className="text-xl font-bold bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent cursor-pointer"
+            >
+              Gajendra
+            </motion.div>
+
+          </a>
+
+        </div>
+
       </div>
-      <div className='flex align-middle justify-center gap-5 text-xl text-white mr-3'>
-        <p>Profile</p>
-        <p>Skills</p>
-        <p>Projects</p>
-      </div>
-    </div>   
-    )
+
+    </nav>
+  )
 };
 
 export default Navbar;
